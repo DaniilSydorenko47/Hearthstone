f5deb8f321661d60ade9863895b95d5943ed69d6
Daniil <ki5896787@gmail.com>
2024-09-28 22:01:00 +0300

    Сервер теперь может переписоваться с клиентом

--- src/main/java/Client2.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.Socket;
-
-public class Client2 {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1",4753);
-
-    }
-}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,19 +1,41 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(4753);
-        Socket inputClient = serverSocket.accept();
-        Scanner in = new Scanner(inputClient.getInputStream());
-        while(in.hasNext()){
-            System.out.println(in.nextLine());
+    public static void main(String[] args) {
+        int port = 8345;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен и ждёт подключений на порту " + port);
+            // Ожидание клиента
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Клиент подключился!");
+            while (true) {
+
+                // Потоки для отправки и получения данных
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // Чтение сообщения от клиента
+                String clientMessage = in.readLine();
+                System.out.println(clientMessage);
+                // Отправка ответа клиенту
+                Scanner sc = new Scanner(System.in);
+                String sentence = sc.nextLine();
+                if (sentence.equals("stop")){
+                    break;
+                }
+                out.println(sentence);
+            }
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка на сервере: " + e.getMessage());
         }
-        in.close();
-        inputClient.close();
-        serverSocket.close();
     }
 }
